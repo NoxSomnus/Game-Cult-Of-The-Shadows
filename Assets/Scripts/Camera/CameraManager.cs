7a3a6b78564a6e25ff3f7471a6ba8c9d12ba2331
Sedet <65431383+sedett@users.noreply.github.com>
2024-06-06 19:31:45 -0400

    limite de camara

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -19,18 +19,21 @@ public class CameraManager : MonoBehaviour
     public bool IsLerpingYDamping { get; private set; }
     public  bool LerpedFromPlayerfalling {  get; set; }
 
-    public Coroutine _lerpYPanCoroutine;
+    private Coroutine _lerpYPanCoroutine;
+    private Coroutine _panCameraCoroutine;
 
-    public CinemachineVirtualCamera _currentCamera;
-    public CinemachineFramingTransposer _framingTransposer;
+    private CinemachineVirtualCamera _currentCamera;
+    private CinemachineFramingTransposer _framingTransposer;
 
-    public float _normYPanAmount;
+    private float _normYPanAmount;
+
+    private Vector2 _startingTrackedObjectOffset;
+
     private void Awake()
     {
         Debug.Log("Awake");
         if (instance == null)
         {
-            Debug.Log("instancia");
 
             instance = this;
         }
@@ -38,7 +41,6 @@ public class CameraManager : MonoBehaviour
         {
             if (_allVirtualCameras[i].enabled)
             {
-                Debug.Log("cosas con la camara");
 
                 //set the current active camera 
                 _currentCamera = _allVirtualCameras[i];
@@ -51,6 +53,9 @@ public class CameraManager : MonoBehaviour
 
         //set the YDamping amount so it's based on the inspector value
         _normYPanAmount = _framingTransposer.m_YDamping;
+
+        //set the starting position of the tracked object offset
+        _startingTrackedObjectOffset = _framingTransposer.m_TrackedObjectOffset;
     }
 
     #region Lerp the Y Damping
@@ -63,7 +68,6 @@ public class CameraManager : MonoBehaviour
     private IEnumerator LerpYAction(bool isPlayerFalling)
     {
         IsLerpingYDamping = true;
-        Debug.Log("corutina        lerp");
 
         // grab the starting damping amount
         float startDampAmount = _framingTransposer.m_YDamping;
@@ -72,14 +76,12 @@ public class CameraManager : MonoBehaviour
         //determine the en damping amount
         if (isPlayerFalling)
         {
-            Debug.Log("estoy cayendo");
 
             endDampAmount = _fallPanAmount;
             LerpedFromPlayerfalling = true;
         }
         else
         {
-            Debug.Log(" no estoy cayendo");
 
             endDampAmount = _normYPanAmount;
         }
@@ -88,7 +90,6 @@ public class CameraManager : MonoBehaviour
         float elapsedTime = 0f;
         while (elapsedTime < _fallYPanTime)
         {
-            Debug.Log("hace cosas en el while");
             elapsedTime += Time.deltaTime;
             
             float lerpedPanAmount = Mathf.Lerp(startDampAmount, endDampAmount,( elapsedTime / _fallYPanTime));
@@ -99,6 +100,68 @@ public class CameraManager : MonoBehaviour
 
 
         IsLerpingYDamping = false;
+    }
+    #endregion
+
+    #region Pan Camera
+
+    public void PanCameraOnContact(float panDistance, float PanTime, PanDirection panDirection, bool panToStartingPos)
+    {
+        _panCameraCoroutine = StartCoroutine(PanCamera(panDistance,PanTime,panDirection,panToStartingPos));
+    }
+
+    private IEnumerator PanCamera (float panDistance, float PanTime, PanDirection panDirection, bool panToStartingPos)
+    {
+        Vector2 endPos = Vector2.zero;
+        Vector2 startingPos = Vector2.zero;
+
+        //handle pan from trigger
+        if (!panToStartingPos)
+        {
+            // set the direction and distance
+            switch (panDirection)
+            {
+                case PanDirection.Up:
+                    endPos = Vector2.up;
+                    break;
+                case PanDirection.Down:
+                    endPos = Vector2.down;
+                    break;
+                case PanDirection.Left:
+                    endPos = Vector2.left;
+                    break;
+                case PanDirection.Right:
+                    endPos = Vector2.right;
+                    break;
+                default:
+                    break;
+            }
+
+            endPos *= panDistance;
+
+            startingPos = _startingTrackedObjectOffset;
+
+            endPos = startingPos;
+        }
+
+        //handle the direction settings whe moving back to the starting position
+        else
+        {
+            startingPos = _framingTransposer.m_TrackedObjectOffset;
+            endPos = _startingTrackedObjectOffset;
+        }
+
+        //handle the actual panning of the camera
+        float elapsedTime = 0f;
+        while (elapsedTime < PanTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            Vector3 panLerp = Vector3.Lerp(startingPos, endPos, (elapsedTime / PanTime));
+            _framingTransposer.m_TrackedObjectOffset = panLerp;
+
+            yield return null;
+        }
     }
     #endregion
 }
