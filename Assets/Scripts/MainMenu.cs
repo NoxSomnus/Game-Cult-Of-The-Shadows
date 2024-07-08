a30d7ec1819ea5bc4fa38ca79e71491d1414c484
Marcos Duque <115253511+Heelioz@users.noreply.github.com>
2024-07-08 00:15:02 -0400

    HotFix

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -20,7 +20,7 @@ public class MainMenu : MonoBehaviour
     {
         src.clip = ConfirmSound;
         src.Play();
-        TransitionManager.Instance.LoadScene(TransitionManager.SCENE_NAME_MAIN_MENU);
+        TransitionManager.Instance.LoadScene("Void Menu");
     }
     public void LoadTutorialScene()
     {
